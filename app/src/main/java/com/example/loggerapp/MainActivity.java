c14577a66c79484c16159b90c27eeab8068044c5
a.suprun <alexander.suprun1@gmail.com>
2021-03-05 15:40:47 +0200

    image capture setup

--- app/src/main/java/com/example/loggerapp/MainActivity.java
+++ app/src/main/java/com/example/loggerapp/MainActivity.java
@@ -1,18 +1,29 @@
 package com.example.loggerapp;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.FileProvider;
 
 import com.bumptech.glide.Glide;
@@ -20,16 +31,24 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.net.URI;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String LOG_FILENAME = "com.example.loggerapp.LOG_FILENAME";
-    private static final int CODE_PICK_PHOTO = 100;
+    private static final int REQUEST_PICK_PHOTO = 100;
+    private static final int REQUEST_IMAGE_CAPTURE = 200;
+    private static final int PERMISSION_REQUEST_CAMERA = 300;
     private boolean isOpen = false;
+    private boolean isCameraAllowed = false;
     private AppCompatImageView imageView;
+    private Uri photoURI;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +56,109 @@ public class MainActivity extends AppCompatActivity {
         writeToFile("onCreate()");
         setupFABMenu();
         imageView = findViewById(R.id.image_view_activity_main_show_image);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            isCameraAllowed = true;
+        } else if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            showRationale();
+        } else {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                isCameraAllowed = true;
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                showRationale();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null) {
+            switch (requestCode) {
+                case REQUEST_PICK_PHOTO:
+                    Glide.with(MainActivity.this)
+                            .load(data.getData())
+                            .into(imageView);
+                    break;
+                case REQUEST_IMAGE_CAPTURE:
+                    Log.i("PHOTO_URI:", photoURI.toString());
+                    Glide.with(MainActivity.this)
+                            .load(photoURI)
+                            .into(imageView);
+            }
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent intentTakePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String imageFileName = "JPEG_" + new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault())
+                .format(new Date()) + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        if (intentTakePicture.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = File.createTempFile(imageFileName, ".jpg", storageDir);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(MainActivity.this,
+                        getString(R.string.provider_authority),
+                        photoFile);
+                intentTakePicture.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                Log.i("PHOTO_URI:", photoURI.toString());
+                startActivityForResult(intentTakePicture, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        writeToFile("onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        writeToFile("onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        writeToFile("onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        writeToFile("onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        writeToFile("onDestroy()");
+    }
+
+    private void writeToFile(String body) {
+        try (FileOutputStream fos = openFileOutput(LOG_FILENAME, Context.MODE_PRIVATE | Context.MODE_APPEND)) {
+            fos.write(('\n' + body).getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void setupFABMenu() {
@@ -78,7 +200,11 @@ public class MainActivity extends AppCompatActivity {
         fabCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Camera clicked", Toast.LENGTH_SHORT).show();
+                if (isCameraAllowed) {
+                    dispatchTakePictureIntent();
+                } else {
+                    showRationale();
+                }
             }
         });
 
@@ -87,7 +213,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intentPickPhoto = new Intent(Intent.ACTION_PICK,
                         MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intentPickPhoto, CODE_PICK_PHOTO);
+                startActivityForResult(intentPickPhoto, REQUEST_PICK_PHOTO);
             }
         });
 
@@ -96,7 +222,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Uri fileUri = FileProvider.getUriForFile(
                         MainActivity.this,
-                        "com.example.loggerapp.fileprovider",
+                        getString(R.string.provider_authority),
                         new File(getFilesDir(), LOG_FILENAME));
 
                 if (fileUri != null) {
@@ -110,51 +236,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CODE_PICK_PHOTO && resultCode == RESULT_OK && data != null) {
-            Glide.with(MainActivity.this)
-                    .load(data.getData())
-                    .into(imageView);
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        writeToFile("onStart()");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        writeToFile("onResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        writeToFile("onPause()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        writeToFile("onStop()");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        writeToFile("onDestroy()");
-    }
-
-    private void writeToFile(String body) {
-        try (FileOutputStream fos = openFileOutput(LOG_FILENAME, Context.MODE_PRIVATE | Context.MODE_APPEND)) {
-            fos.write(('\n' + body).getBytes());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void showRationale() {
+        new AlertDialog.Builder(this)
+                .setMessage(R.string.message_rationale)
+                .setPositiveButton(getString(R.string.button_title_allow), new DialogInterface.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.M)
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        requestPermissions(new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+                    }
+                })
+                .setNegativeButton(getString(R.string.button_title_block), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        isCameraAllowed = false;
+                    }
+                })
+                .create()
+                .show();
     }
 }
